e71d34da27a51ff23221173f3c8b5761992ab706
Matheus Hahn <hahnnmatheus@gmail.com>
2016-07-05 23:14:02 -0300

    Working with  vs computed properties

--- computedProperties/js/js.js
+++ computedProperties/js/js.js
@@ -1,45 +1,55 @@
-
-
+/*
 var vm = new Vue({
   el: '#app',
   data: {
-    titleIndex: 'Treine seus conhecimento da tabuada',
-    home: true, // Show home.
-    challenge: false, // Hide challenge.
-    val1: null,
-    val2: null,
-    total: null
-  },    
+    a: ,
+    c: 2
+  },
+  computed: {
+    // a computed getter
+    b: function () {
+      // `this` points to the vm instance
+      return this.a + 
+    },
+    d: function () {
+      // error with extensive numbers
+      return ( parseInt(this.c) + 0 );
+    }
+  }
 })
 
-vm.selectNumbers = function(number) {
-  number = parseInt(number);
-  switch(number) {
-    case 0:
-      vm.home = false; // Hide home view.
-      vm.challenge = true; // Show challenge.
-      var random = Math.floor(Math.random() * (1 - 0 + 1)) + 0; // Random chooses test.
-      if (random) {
-        // A random number is chossen for val1 and val2 inputs.
-        console.info('true' + random);
-        vm.val1 = vm.total = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-        vm.val2 = vm.total = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-      } else {
-        // A random number is chossen for total input..
-        console.info('false' + random);
-        vm.total = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-      }
+*/
+/*
 
-    break;
-    case 1:
-      vm.home = false;
-      vm.challenge = true;
-    break;
-    case 2:
-
-    break;
-    case 3:
-
-    break;
+-- With $watch -- 
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
   }
-}
+})
+
+vm.$watch('firstName', function (val) {
+  this.fullName = val + ' ' + this.lastName
+})
+
+vm.$watch('lastName', function (val) {
+  this.fullName = this.firstName + ' ' + val
+})
+
+*/
+// - with computed properties
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
